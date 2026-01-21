c2eb0bb92301e9660bcf7968ec690fe48a5b72c4
r-dev95 <r95.dev95@gmail.com>
2026-01-21 15:54:09 +0900

    feat: DI機能の追加

--- RecordingApp/App.xaml.cs
+++ RecordingApp/App.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +14,7 @@ namespace RecordingApp
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
         private Window? _window;
 
         /// <summary>
@@ -35,6 +24,27 @@ namespace RecordingApp
         public App()
         {
             InitializeComponent();
+
+            _host = Host
+                .CreateDefaultBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<MainWindow>();
+                })
+                .Build();
+        }
+
+        public static T GetService<T>()
+            where T : class
+        {
+            if ((Current as App)!._host.Services.GetService(typeof(T)) is not T service)
+            {
+                string msg = $"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.";
+                throw new ArgumentException(msg);
+            }
+
+            return service;
         }
 
         /// <summary>
@@ -43,7 +53,7 @@ namespace RecordingApp
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            _window = new MainWindow();
+            _window = GetService<MainWindow>();
             _window.Activate();
         }
     }
