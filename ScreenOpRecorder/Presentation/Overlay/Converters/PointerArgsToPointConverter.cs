61d99f55a8355f157aa2f0bea81d10b13046b3fe
r-dev95 <r95.dev95@gmail.com>
2026-03-02 19:15:22 +0900

    feat: PointerArgsToPointConverterのnull/型不一致を安全に処理

--- ScreenOpRecorder/Presentation/Overlay/Converters/PointerArgsToPointConverter.cs
+++ ScreenOpRecorder/Presentation/Overlay/Converters/PointerArgsToPointConverter.cs
@@ -9,8 +9,12 @@ namespace ScreenOpRecorder.Presentation.Overlay.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var args = value as PointerRoutedEventArgs;
-            var element = args!.OriginalSource as UIElement;
+            if (value is not PointerRoutedEventArgs args)
+            {
+                return new Windows.Foundation.Point();
+            }
+
+            var element = args.OriginalSource as UIElement;
             return args.GetCurrentPoint(element).Position;
         }
 
