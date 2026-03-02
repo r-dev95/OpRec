2ed6c5eab530c7457639162dd1fe1020b42efe21
r-dev95 <r95.dev95@gmail.com>
2026-03-02 17:15:50 +0900

    feat: EventBusの並行処理を整理し、配信継続性とDispose安全性を改善

--- ScreenOpRecorder/Infrastructure/Events/EventBus.cs
+++ ScreenOpRecorder/Infrastructure/Events/EventBus.cs
@@ -1,69 +1,72 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
 using ScreenOpRecorder.Application.Events.Ports;
 
 namespace ScreenOpRecorder.Infrastructure.Events
 {
     public class EventBus : IEventBus
     {
-        private readonly ConcurrentDictionary<Type, List<Delegate>> _handlers = new();
-        private readonly object _gate = new();
+        private readonly ConcurrentDictionary<Type, ConcurrentDictionary<Delegate, byte>> _handlers = new();
 
         public IDisposable Subscribe<T>(Action<T> handler)
         {
-            lock (_gate)
-            {
-                if (!_handlers.TryGetValue(typeof(T), out var list))
-                {
-                    list = new List<Delegate>();
-                    _handlers[typeof(T)] = list;
-                }
-                list.Add(handler);
-            }
+            var bucket = _handlers.GetOrAdd(typeof(T), _ => new ConcurrentDictionary<Delegate, byte>());
+            bucket.TryAdd(handler, 0);
 
             return new Subscription(() => Unsubscribe(handler));
         }
 
         public void Publish<T>(T message)
         {
-            Delegate[] snapshot;
-            lock (_gate)
+            if (!_handlers.TryGetValue(typeof(T), out var bucket) || bucket.IsEmpty)
             {
-                if (!_handlers.TryGetValue(typeof(T), out var list) || list.Count == 0)
-                {
-                    return;
-                }
-                snapshot = list.ToArray();
+                return;
             }
+
+            Exception? firstException = null;
+            var snapshot = bucket.Keys.ToArray();
 
             foreach (var handler in snapshot)
             {
-                ((Action<T>)handler).Invoke(message);
+                try
+                {
+                    ((Action<T>)handler).Invoke(message);
+                }
+                catch (Exception ex)
+                {
+                    firstException ??= ex;
+                }
+            }
+
+            if (firstException != null)
+            {
+                throw firstException;
             }
         }
 
         private void Unsubscribe<T>(Action<T> handler)
         {
-            lock (_gate)
+            if (!_handlers.TryGetValue(typeof(T), out var bucket))
             {
-                if (!_handlers.TryGetValue(typeof(T), out var list))
-                {
-                    return;
-                }
+                return;
+            }
 
-                list.Remove(handler);
-                if (list.Count == 0)
-                {
-                    _handlers.TryRemove(typeof(T), out _);
-                }
+            bucket.TryRemove(handler, out _);
+
+            if (bucket.IsEmpty)
+            {
+                _handlers.TryRemove(new KeyValuePair<Type, ConcurrentDictionary<Delegate, byte>>(typeof(T), bucket));
             }
         }
 
         private sealed class Subscription : IDisposable
         {
             private readonly Action _disposeAction;
-            private bool _disposed;
+            private int _disposed;
 
             public Subscription(Action disposeAction)
             {
@@ -72,13 +75,12 @@ namespace ScreenOpRecorder.Infrastructure.Events
 
             public void Dispose()
             {
-                if (_disposed)
+                if (Interlocked.Exchange(ref _disposed, 1) != 0)
                 {
                     return;
                 }
 
                 _disposeAction();
-                _disposed = true;
             }
         }
     }
